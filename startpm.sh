aff32d85b2339845b9b6fba027157da1ec29ab5b
Juuso Vilmunen <juuso@vilmunen.net>
2017-09-23 15:38:21 +0300

    watch only server.js

--- startpm.sh
+++ startpm.sh
@@ -1,3 +1,3 @@
-./node_modules/pm2/bin/pm2 start --no-daemon -i 1 --watch . --name=my-process server.js
+./node_modules/pm2/bin/pm2 start --no-daemon -i 1 --watch server.js --name=my-process server.js
 ./node_modules/pm2/bin/pm2 log
 
