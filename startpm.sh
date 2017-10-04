ca38aac06ebe3febaff1aca6900392e6920f7325
Juuso Vilmunen <juuso@vilmunen.net>
2017-10-04 15:54:54 +0300

    pm2 start only

--- startpm.sh
+++ startpm.sh
@@ -1,8 +1,3 @@
-./node_modules/pm2/bin/pm2 stop all || true
-./node_modules/pm2/bin/pm2 kill || true
-./node_modules/pm2/bin/pm2 start ecosystem.config.js
-#--no-daemon -i 1 --watch --name=tzm-bot server.js
-
 if [ ! -d node_modules/discord.js ]; then npm install discord.js; fi
 if [ ! -d node_modules/pm2 ]; then npm install pm2; fi
 if [ ! -d node_modules/sleep ]; then npm install sleep; fi
@@ -16,6 +11,6 @@ if [ ! -d podbot ]; then
 	git clone https://github.com/tzmfi/podbot.git
 fi
 
-./node_modules/pm2/bin/pm2 log
+./node_modules/pm2/bin/pm2 start ecosystem.config.js
 
 
