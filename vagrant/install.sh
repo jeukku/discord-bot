9f5040274171fcace45e846748548e8061bb8f72
Juuso Vilmunen <juuso@vilmunen.net>
2017-12-19 09:18:01 +0200

    install

--- vagrant/install.sh
+++ vagrant/install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
-sudo apt-get install -y nodejs
+curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
+sudo apt-get install -y npm
 sudo apt-get install -y mongodb
